cef86fa8c6dba8b961656df1e0c3db906bed78ef
Cemre Fatih Karakulak <cradexco@gmail.com>
2020-10-27 22:47:04 +0300

    API

--- server.py
+++ server.py
@@ -1,16 +1,20 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 import requests
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET','POST'])
+@app.route('/', methods=['POST'])
 def home():
-    data = request.get_json(force=True)
+    data = request.get_json(force=False)
     
-    tag = data['tag']
-    
-    return tag
-    
+    try:
+        tag = data['tag']
+    except:
+        return jsonify({
+            "success": False,
+            "error": "Missing parameter: tag"
+        })
+
     headers = {
         "User-agent": "zinc-awx-client",
         "Content-Type": "application/json",
@@ -19,8 +23,14 @@ def home():
 
     templates = requests.get("http://34.123.174.145/api/v2/job_templates", headers=headers)
     
-    job_id = [x['id'] for x in templates.json()['results'] if x['name'] == 'JL2 Goto'][0]
-    
+    try:
+        job_id = [x['id'] for x in templates.json()['results'] if x['name'] == 'JL2 Goto'][0]
+    except:
+        return jsonify({
+            "success": False,
+            "error": "Can't find job id for JL2 Goto"
+        })
+        
     launch = requests.post(
         url=f"http://34.123.174.145/api/v2/job_templates/{job_id}/launch",
         headers=headers,
@@ -32,4 +42,4 @@ def home():
     )
     
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0')
