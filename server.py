aa92407997f5bb5f952335ab356a071d9037f3f4
Cemre Fatih Karakulak <cradexco@gmail.com>
2020-11-02 23:52:39 +0300

    channels

--- server.py
+++ server.py
@@ -5,14 +5,39 @@ app = Flask(__name__)
 
 URL = "http://34.123.174.145"
 
+ALLOWED_CHANNELS = {
+    'jl-deploys': 'G01D6U7BWTW',
+}
+
+ALLOWED_USERS = {
+    'cemre': 'U010V29AVSA',
+    'eric': 'U0ELWGVGT',
+    'oz': 'UJ0A0J4LX',
+}
+
 @app.route('/', methods=['POST'])
 def home():
-    text = request.form.get('text', None)
+    user_id = request.form.get('user_id')
+    channel_id = request.form.get('channel_id')
     
+    if not user_id in ALLOWED_USERS.values():
+        return jsonify({
+            "response_type": "in_channel",
+            "text": f"You do not have permission to run this command",
+        })
+    
+    if not channel_id in ALLOWED_CHANNELS.values():
+        return jsonify({
+            "response_type": "in_channel",
+            "text": f"This command is only available in #jl-deploys channel",
+        })
+        
+    text = request.form.get('text', None)
+        
     if not text:
         return jsonify({
-            "success": False,
-            "error": "Missing parameter: text"
+            "response_type": "in_channel",
+            "error": "Missing parameter: text",
         })
         
     params = text.split(' ')
@@ -22,14 +47,14 @@ def home():
         TAG = str(params[1])
     except:
         return jsonify({
-            "success": False,
+            "response_type": "in_channel",
             "error": "Missing parameter: JOB or TAG"
         })
 
     headers = {
         "User-Agent": "zinc-awx-client",
         "Content-Type": "application/json",
-        "Authorization": "Basic YWRtaW46YWs2NGNrOTQ=" # dont store it like this on production
+        "Authorization": "Basic ZGVwbG95Ym90OjQ4TG1XdnJSOTZhTXhhUkh1bVE3VldqOUZ1eVVVUHVE" # deploybot cant do much tho @cemre change it pls.
     }
 
     templates = requests.get(
@@ -41,8 +66,8 @@ def home():
         job_id = [x['id'] for x in templates.json()['results'] if x['name'] == (JOB + 'JumpToSpecificVersion')][0]
     except:
         return jsonify({
-            "success": False,
-            "error": f"Can't find job id for {JOB}Jump"
+            "response_type": "in_channel",
+            "error": f"Can't find job id for {JOB}JumpToSpecificVersion"
         })
         
     awx_data = {
